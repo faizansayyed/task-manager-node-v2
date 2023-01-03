2ba286a10cc00dd24c0cb0962716ea984e1bfe4c
Faizan Sayyed <faizansayyed36@gmail.com>
2023-01-03 10:50:41 +0530

    completed auth req validation

--- src/routes/v1/auth.router.js
+++ src/routes/v1/auth.router.js
@@ -1,48 +1,58 @@
 const express = require("express");
 const { authController } = require("../../controllers");
-const validateRequest = require("../../middlewares/validate");
+const validate = require("../../middlewares/validate");
 const catchAsync = require("../../utils/catchAsync");
 const auth = require("../../middlewares/auth");
+const { authValidation } = require("../../validations");
 
 const router = express.Router();
 
 router.post(
   "/register",
-  validateRequest(),
+  validate(authValidation.register),
   catchAsync(authController.register)
 );
 
-router.post("/login", validateRequest(), catchAsync(authController.login));
+router.post(
+  "/login",
+  validate(authValidation.login),
+  catchAsync(authController.login)
+);
 router.post(
   "/logout",
   auth,
-  validateRequest(),
+  validate(authValidation.logout),
   catchAsync(authController.logout)
 );
 
 router.post(
   "/refresh-tokens",
-  validateRequest(),
+  validate(authValidation.refreshTokens),
   catchAsync(authController.refreshTokens)
 );
 router.post(
   "/forgot-password",
-  validateRequest(),
+  validate(authValidation.forgotPassword),
   catchAsync(authController.forgotPassword)
 );
 
 router.post(
   "/reset-password",
-  validateRequest(),
+  validate(authValidation.resetPassword),
   catchAsync(authController.resetPassword)
 );
 
 router.post(
   "/send-verification-email",
   auth,
+  validate(),
   authController.sendVerificationEmail
 );
 
-router.post("/verify-email", validateRequest(), authController.verifyEmail);
+router.post(
+  "/verify-email",
+  validate(authValidation.verifyEmail),
+  authController.verifyEmail
+);
 
 module.exports = router;
